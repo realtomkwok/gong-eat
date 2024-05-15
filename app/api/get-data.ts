498d38957a8db7875740a15952ce392c6264c9d5
Tom Kwok <work@tomkwok.xyz>
2024-05-15 21:00:13 +1000

    Update Dockerfile

--- app/api/get-data.ts
+++
@@ -1,9 +0,0 @@
-export default async function getData(url: string) {
-    // The return value is *not* serialized
-
-    const response = await fetch(process.env.LOCALHOST_URL + url, {method: 'GET'})
-    if (!response.ok) {
-        throw new Error(`Failed to fetch data. HTTP status: ${response.status}`)
-    }
-    return response.json()
-}
---
+++ app/api/restaurant-categories.ts
@@ -0,0 +1,72 @@
+import {CategoryData} from "@/app/api/definitions";
+
+export const RestaurantCategories: CategoryData[] = [
+        {
+            category_id: 1,
+            category_name: "Chinese",
+            category_icon: "🥡",
+            category_slug: "/category/chinese"
+        },
+        {
+            category_id: 2,
+            category_name: "Indian",
+            category_icon: "🍛",
+            category_slug: "/category/indian"
+        },
+        {
+            category_id: 3,
+            category_name: "Italian",
+            category_icon: "🍝",
+            category_slug: "/category/italian"
+        },
+        {
+            category_id: 4,
+            category_name: "Japanese",
+            category_icon: "🍱",
+            category_slug: "/category/japanese"
+        },
+        {
+            category_id: 5,
+            category_name: "Mexican",
+            category_icon: "🌮",
+            category_slug: "/category/mexican"
+        },
+        {
+            category_id: 6,
+            category_name: "Thai",
+            category_icon: "🥘",
+            category_slug: "/category/thai"
+        },
+        {
+            category_id: 7,
+            category_name: "Pizza",
+            category_icon: "🍕",
+            category_slug: "/category/pizza"
+        },
+        {
+            category_id: 8,
+            category_name: "Fast Food",
+            category_icon: "🍔",
+            category_slug: "/category/burgers"
+        },
+        {
+            category_id: 9,
+            category_name: "Bakery",
+            category_icon: "🥐",
+            category_slug: "/category/bakery"
+        },
+        {
+            category_id: 10,
+            category_name: "Vegan",
+            category_icon: "🥬",
+            category_slug: "/category/vegan"
+        },
+        {
+            category_id: 11,
+            category_name: "Healthy",
+            category_icon: "🥦",
+            category_slug: "/category/healthy"
+        }
+    ]
+
+export default RestaurantCategories
